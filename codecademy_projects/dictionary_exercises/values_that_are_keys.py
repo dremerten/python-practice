e6b6bddf46e4b87cee001c948858d815791a44f7
Andre Merten <dremer415@gmail.com>
2026-03-02 08:08:23 -0800

    refine

--- codecademy_projects/dictionary_exercises/values_that_are_keys.py
+++ codecademy_projects/dictionary_exercises/values_that_are_keys.py
@@ -11,12 +11,11 @@ We are making a program that will create a family tree. Using a dictionary, we w
 """
 
 def values_that_are_keys(my_dictionary):
-    dict_values = []
-    for value in my_dictionary.values():
-        for key in my_dictionary.keys():
-            if value is key:
-                dict_values.append(key)
-    return dict_values
+  value_keys = []
+  for value in my_dictionary.values():
+    if value in my_dictionary:
+      value_keys.append(value)
+  return value_keys
 
 print(values_that_are_keys({1:100, 2:1, 3:4, 4:10})) # should print ==> [1, 4]
 print(values_that_are_keys({"a":"apple", "b":"a", "c":100})) # should print ===> ['a']

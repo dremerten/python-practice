7d4772168d2bedcb5b4a24f9003cbcb5c496133d
Andre Merten <dremer415@gmail.com>
2026-03-04 13:09:05 -0800

    function as a return value example

---
+++ python-intermediate/higher_order_functions/function_as_return_value.py
@@ -0,0 +1,12 @@
+'''
+defines and returns a function that takes two numeric arguments,length &  width. 
+returns the volume given the input height
+'''
+def make_box_volume_function(height):
+
+    def volume(length, width):
+        return length*width*height
+    return volume
+ 
+box_volume_height15 = make_box_volume_function(15)
+print(box_volume_height15(3,2))

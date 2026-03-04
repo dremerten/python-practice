02c50542187e28a462a10dd2a0111161f8d35dc1
Andre Merten <dremer415@gmail.com>
2026-03-04 13:10:27 -0800

    function as a return value example

--- python-intermediate/higher_order_functions/function_as_return_value.py
+++ python-intermediate/higher_order_functions/function_as_return_value.py
@@ -1,6 +1,13 @@
 '''
 defines and returns a function that takes two numeric arguments,length &  width. 
 returns the volume given the input height
+
+In the example, we wrote a higher-order function, make_box_volume_function(), 
+that takes a height as an argument and returns a new function that calculates the volume 
+of any box with that height when it is passed the length and width of the box. 
+As we can see, higher-order functions with functions as return values are just as reusable as higher-order
+functions with functions as arguments and, therefore, also reduce repetition 
+and the chances for mistakes to creep into code.
 '''
 def make_box_volume_function(height):
 

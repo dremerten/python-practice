b36326bc7f68595aed9e56e3999acd5e3e1c5042
Andre Merten <dremer415@gmail.com>
2026-03-04 10:02:09 -0800

    local scope example script

---
+++ python-intermediate/namespaces/local_scope_example.py
@@ -0,0 +1,7 @@
+def painting(paint_colors, picture):
+  painting_statement = "To paint the " + picture + " we need the following colors: "
+  print(painting_statement)
+  for color in paint_colors:
+      print(color)
+  print(locals())
+painting(['Saffron', 'White', 'Green'], 'Indian Flag')

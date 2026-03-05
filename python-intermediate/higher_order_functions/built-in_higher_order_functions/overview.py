0b14a41c2255367253ab8113c8bde4b80a91fe47
Andre Merten <dremer415@gmail.com>
2026-03-05 12:00:51 -0800

    overivew of built-in functions

---
+++ python-intermediate/higher_order_functions/built-in_higher_order_functions/overview.py
@@ -0,0 +1,7 @@
+"""
+
+The map() function applies a passed function to each element in an iterable and returns a map object.
+The filter() function applies a filtering function (a function that returns a boolean) to each element in an iterable. filter() returns a filter object with only the elements for which the filtering function returned True.
+reduce() must be imported from the functools module. It reduces an iterable to a single value by cumulatively applying a passed function to the first pair of elements in the iterable and then each sequential element with the return value.
+These three functions streamline code on their own, but they are even easier to read when they are used in conjunction with lambda functions.
+"""

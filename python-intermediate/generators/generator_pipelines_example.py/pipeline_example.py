38ed8738e0515819b76560d60ab6de9501c89cc4
Andre Merten <dremer415@gmail.com>
2026-03-09 21:02:51 -0700

    pipeline example

---
+++ python-intermediate/generators/generator_pipelines_example.py/pipeline_example.py
@@ -0,0 +1,17 @@
+def number_generator():
+  i = 0
+  while True:
+    yield i
+    i += 1
+    
+def even_number_generator(numbers):
+  for n in numbers:
+    if n % 2 == 0:
+      yield n
+
+even_numbers = even_number_generator(number_generator())
+
+for e in even_numbers:
+  print(e)
+  if e == 40000:
+    break

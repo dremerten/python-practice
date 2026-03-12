ca4f90f59586b9e5daa5cebf2f15be8a27526cfb
Andre Merten <dremer415@gmail.com>
2026-03-12 13:09:48 -0700

    add contextlib example

--- python-intermediate/context_managers/Contextlib/contextlib_example.py
+++ python-intermediate/context_managers/Contextlib/contextlib_example.py
@@ -11,17 +11,33 @@ def generator_function(<parameters>):
 """
 
 
+# @contextmanager
+# def poem_files(file, mode):
+#   print("Opening File")
+#   open_poem_file = open(file, mode)
+#   try:
+#     yield open_poem_file
+#   finally:
+#     print("Closing File")
+#     open_poem_file.close()
+
+
+# with poem_files('poem.txt', 'a') as opened_file:
+#  print('Inside yield')
+#  opened_file.write('Rose is beautiful, Just like you.')
+
+from contextlib import contextmanager
+
 @contextmanager
-def poem_files(file, mode):
-  print("Opening File")
-  open_poem_file = open(file, mode)
-  try:
-    yield open_poem_file
-  finally:
-    print("Closing File")
-    open_poem_file.close()
+def open_file_contextlib(file, mode):
+    open_file = open(file, mode)
+    try:
+        yield open_file
+    # Exception Handling
+    except Exception as exception:
+        print('We hit an error: ' + str(exception))
+    finally:
+        open_file.close()
 
-
-with poem_files('poem.txt', 'a') as opened_file:
- print('Inside yield')
- opened_file.write('Rose is beautiful, Just like you.')
+with open_file_contextlib('file.txt', 'w') as opened_file:
+    opened_file.sign('We just made a context manager using contexlib')

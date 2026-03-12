95352714b934068b010bcf6c16e7c6a57cd9cabf
Andre Merten <dremer415@gmail.com>
2026-03-12 12:18:31 -0700

    add custom context manager class and Contextlib examples

---
+++ python-intermediate/context_managers/Contextlib/contextlib_example.py
@@ -0,0 +1,27 @@
+from contextlib import contextmanager
+
+"""
+@contextmanager
+def generator_function(<parameters>):
+    <setup section - equivalent to __enter__ >
+    try:
+        yield <value>
+    finally:
+        <cleanup section - equivalent to __exit__ >
+"""
+
+
+@contextmanager
+def poem_files(file, mode):
+  print("Opening File")
+  open_poem_file = open(file, mode)
+  try:
+    yield open_poem_file
+  finally:
+    print("Closing File")
+    open_poem_file.close()
+
+
+with poem_files('poem.txt', 'a') as opened_file:
+ print('Inside yield')
+ opened_file.write('Rose is beautiful, Just like you.')

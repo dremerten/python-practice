9fa9c3f301d1d75724bf04aea3f9319624d3bec4
Andre Merten <dremer415@gmail.com>
2026-03-15 16:55:59 -0700

    namedtuple and lambda examples

--- python-advanced/functional_programming/lambda_functions/lambda_example2.py
+++ python-advanced/functional_programming/lambda_functions/lambda_example2.py
@@ -10,3 +10,8 @@ def odd_or_even(n):
     square = lambda x: x*x
     cube = lambda x: x*x*x
     return square(n) if n % 2 == 0 else cube(n)
+
+squared_num = odd_or_even(20)
+cubed_num = odd_or_even(11)
+print(f"Squared: {squared_num}")
+print(f"Cubed: {cubed_num}")

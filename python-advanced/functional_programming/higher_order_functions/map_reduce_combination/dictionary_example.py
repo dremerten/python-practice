cdfa33a77bbe965ce13ebb830865df5d95115377
Andre Merten <dremer415@gmail.com>
2026-03-16 11:12:21 -0700

    dictionary example

--- python-advanced/functional_programming/higher_order_functions/map_reduce_combination/dictionary_example.py
+++ python-advanced/functional_programming/higher_order_functions/map_reduce_combination/dictionary_example.py
@@ -7,14 +7,14 @@ fruits = {"Grape": (4, 6, 2),
           "Watermelon": (0, 9, 6)}
 
 # 1 Original version using reduce + map
-total_fruits_1 = reduce(lambda x, y: x + y, map(lambda q: fruits[q][0] + fruits[q][1] + fruits[q][2], fruits))
+total_fruits_1 = reduce(lambda x, y: x + y, map(lambda key: fruits[key][0] + fruits[key][1] + fruits[key][2], fruits))
 print("Original reduce+map:", total_fruits_1)
 
 # 2 Generator comprehension version with reduce
-total_fruits_2 = reduce(lambda x, y: x + y, (sum(fruits[q]) for q in fruits))
+total_fruits_2 = reduce(lambda x, y: x + y, (sum(fruits[key]) for key in fruits))
 print("Generator comprehension + reduce:", total_fruits_2)
 
 # 3 Pythonic version using just sum()
-total_fruits_3 = sum(sum(fruits[q]) for q in fruits)
+total_fruits_3 = sum(sum(fruits[key]) for key in fruits)
 print("Pythonic sum():", total_fruits_3)
 

24bf0cb05a6026ee21581a91fa1943e2f26d0af7
Andre Merten <dremer415@gmail.com>
2026-03-19 13:55:54 -0700

    add thread and seqential examples

---
+++ python-advanced/conncurent_programming/sequential_example.py
@@ -0,0 +1,12 @@
+import time
+
+
+def sequential():
+  s = time.perf_counter()
+  print("Codecademy")
+  time.sleep(2)
+  print("says hello!")
+  elapsed = time.perf_counter() - s
+  print("Sequential Programming Elapsed Time: " + str(elapsed) + " seconds")
+
+sequential()
